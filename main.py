f6e38e8db1bbba57500bbd69594a838771f593ca
Parlatore <vcarraly@gmail.com>
2026-02-22 00:06:13 -0300

    Major changes to layout and reorganized buttons logic

--- main.py
+++ main.py
@@ -1,61 +1,77 @@
 import customtkinter
 
 class Button(customtkinter.CTkButton):
-    def __init__(self, master, value, posiX, posiY):
+    def __init__(self, value, posiX, posiY):
+
+        if value.isdigit() or value == ",":
+            back_color = "#525150"
+            txt_color = "#ffffff"
+            hv_color = "#363534"
+        elif value == "=":
+            back_color = "#d47e06"
+            txt_color = "#FFFFFF"
+            hv_color = "#905503"
+        else:
+            back_color = "#c4c3c0"
+            txt_color = "#525150"
+            hv_color = "#747471"
+
+        super().__init__(app, text=value, font=("arial",25), command=self.button_callback, width=70,
+                        height=70, fg_color=back_color, text_color=txt_color, hover_color=hv_color)
+        self.grid(row=posiX, column=posiY, padx=2, pady=2, sticky="nsew")
 
         self.value = value
-
-        super().__init__(master, text=value, command=self.button_callback, width=70, height=70)
-        self.grid(row=posiX, column=posiY, padx=2, pady=2, sticky="nsew")
-        
 
     def button_callback(self):
         print(f"button {self.value} pressed")
 
 class Label(customtkinter.CTkLabel):
-    def __init__(self, master):
-        super().__init__(master, width=400, corner_radius=0, height=100, anchor="e", padx=8)
-        
-        self.grid(row=0, column=0, columnspan=4, sticky="nsew")
+    def __init__(self, master, posiX, posiY, label_size, font_size):
+        super().__init__(master, height=label_size, anchor="e", padx=10, font=("arial", font_size))
+
+        self.grid(row=posiX, column=posiY, columnspan=4, sticky="nsew")
         self.configure(text = "Some example text!")
 
-
+customtkinter.set_appearance_mode("dark")
 app = customtkinter.CTk()
 app.title("my app")
 app.geometry("300x480")
+app.minsize(width=200, height=300)
 
 for i in range(4):
     app.grid_columnconfigure(i, weight=1)
-    
+
 for i in range(6):
     app.grid_rowconfigure(i, weight=1)
 
-# region Numbered Buttons
-button_9 = Button(app, "9", 2, 0)
-button_8 = Button(app, "8", 2, 1)
-button_7 = Button(app, "7", 2, 2)
-button_6 = Button(app, "6", 3, 0)
-button_5 = Button(app, "5", 3, 1)
-button_4 = Button(app, "4", 3, 2)
-button_3 = Button(app, "3", 4, 0)
-button_2 = Button(app, "2", 4, 1)
-button_1 = Button(app, "1", 4, 2)
-button_0 = Button(app, "0", 5, 1)
-# endregion
+Input = Label(app, 0, 0, 60, 35)
+Output = Label(app, 1, 0, 40, 20)
 
-# region Operation Buttons
-button_AC = Button(app, "AC", 1, 0)
-button_Percent = Button(app, "%", 1, 1)
-button_Del = Button(app, "Del", 1, 2)
-button_Div = Button(app, "÷", 1, 3)
-button_Mult = Button(app, "X", 2, 3)
-button_Minus = Button(app, "-", 3, 3)
-button_Plus = Button(app, "+", 4, 3)
-button_Double0 = Button(app, "00", 5, 0)
-button_Comma = Button(app, ",", 5, 2)
-button_Equal = Button(app, "=", 5, 3)
-# endregion
+button_layouts = { # value, posiX, posiY
+    "b9": ("9", 3, 0),
+    "b8": ("8", 3, 1),
+    "b7": ("7", 3, 2),
+    "b6": ("6", 4, 0),
+    "b5": ("5", 4, 1),
+    "b4": ("4", 4, 2),
+    "b3": ("3", 5, 0),
+    "b2": ("2", 5, 1),
+    "b1": ("1", 5, 2),
+    "b0": ("0", 6, 1),
+    "AC": ("AC", 2, 0),
+    "percent": ("%", 2, 1),
+    "del": ("Del", 2, 2),
+    "div": ("÷", 2, 3),
+    "mult": ("x", 3, 3),
+    "minus": ("–", 4, 3),
+    "plus": ("+", 5, 3),
+    "double0": ("00", 6, 0),
+    "comma": (",", 6, 2),
+    "equal": ("=", 6, 3)
+}
 
-text = Label(app)
+for layout in button_layouts.items():
+    val, x, y = layout[1]
+    Button(val, x, y)
 
 app.mainloop()
